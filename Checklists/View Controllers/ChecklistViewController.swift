d9795355c70cfae83ddf6927132c6c312170fa85
Wanguy <w020283852@gmail.com>
2018-01-06 20:24:45 +0800

    UI improvement

--- Checklists/View Controllers/ChecklistViewController.swift
+++ Checklists/View Controllers/ChecklistViewController.swift
@@ -1,32 +1,32 @@
 /**
- * Copyright (c) 2017 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+* Copyright (c) 2017 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+* distribute, sublicense, create a derivative work, and/or sell copies of the
+* Software in any work that is designed, intended, or marketed for pedagogical or
+* instructional purposes related to programming, coding, application development,
+* or information technology.  Permission for such use, copying, modification,
+* merger, publication, distribution, sublicensing, creation of derivative works,
+* or sale is expressly withheld.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 
 import UIKit
 
@@ -39,11 +39,11 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     navigationItem.largeTitleDisplayMode = .never
     title = checklist.name
   }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
   
   // MARK:- Navigation
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -65,6 +65,7 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
   func configureCheckmark(for cell: UITableViewCell,
                           with item: ChecklistItem) {
     let label = cell.viewWithTag(1001) as! UILabel
+    label.textColor = view.tintColor
     if item.checked {
       label.text = "√"
     } else {
@@ -77,7 +78,7 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     let label = cell.viewWithTag(1000) as! UILabel
     label.text = item.text
   }
-  
+    
   // MARK:- TableView Delegates
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return checklist.items.count
